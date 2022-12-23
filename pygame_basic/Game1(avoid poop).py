2a2a9b81fdcadc7129210ddaf7d2e649ded2209b
2jonghoon <bwkw@naver.com>
2022-12-23 18:10:21 +0900

    똥 for문 돌려 여러개 생성하기 ver1

--- pygame_basic/Game1(avoid poop).py
+++ pygame_basic/Game1(avoid poop).py
@@ -54,6 +54,28 @@ character_y_pos = screen_height - character_height
 # 캐릭터의 이동 속도
 character_speed = 0.6
 
+# 똥 리스트로 구하기
+
+def create_ddong(value):
+  ddong = []
+  for i in range(value):
+    ddong.append("ddong"+str(i))
+  return ddong
+
+
+dd = create_ddong(5)
+
+print(dd)
+
+for dong in dd:
+  dong = pygame.image.load("D:\\sourceTree\\Pythone_Project1(Game)\\pygame_basic\\ddong.png")
+  dong_size = dong.get_rect().size
+  dong_width = dong_size[0]
+  dong_height = dong_size[1]
+  
+  print(dong_width)
+
+
 # 똥 그리기
 ddong = pygame.image.load("D:\\sourceTree\\Pythone_Project1(Game)\\pygame_basic\\ddong.png")
 # 똥 사이즈 가져오기
@@ -67,14 +89,40 @@ ddong_y_pos = 0
 # 똥 내려오는 속도
 ddong_speed = 15
 
+# 똥 그리기
+ddong1 = pygame.image.load("D:\\sourceTree\\Pythone_Project1(Game)\\pygame_basic\\ddong.png")
+# 똥 사이즈 가져오기
+ddong1_size = ddong1.get_rect().size
+ddong1_width = ddong1_size[0]
+ddong1_height = ddong1_size[1]
+# 똥 위치 지정 (랜덤함수 사용)
+ddong1_x_pos = randint(0, (screen_width - ddong1_width))
+ddong1_y_pos = 0
+
+# 똥 내려오는 속도
+ddong1_speed = 20
+
+# 똥 그리기
+ddong2 = pygame.image.load("D:\\sourceTree\\Pythone_Project1(Game)\\pygame_basic\\ddong.png")
+# 똥 사이즈 가져오기
+ddong2_size = ddong2.get_rect().size
+ddong2_width = ddong2_size[0]
+ddong2_height = ddong2_size[1]
+# 똥 위치 지정 (랜덤함수 사용)
+ddong2_x_pos = randint(0, (screen_width - ddong2_width))
+ddong2_y_pos = 0
+
+# 똥 내려오는 속도
+ddong2_speed = 7
+
 # 코인 그리기 2022.12.22
 coin = pygame.image.load("D:\\sourceTree\\Pythone_Project1(Game)\\pygame_basic\\coin.png")
 # 코인 사이즈 가져오기
 coin_size = coin.get_rect().size
-coin_witdh = coin_size[0]
+coin_width = coin_size[0]
 coin_height = coin_size[1]
 # 코인 위치 지정
-coin_x_pos = randint(0, (screen_width - coin_witdh))
+coin_x_pos = randint(0, (screen_width - coin_width))
 coin_y_pos = 0
 
 # 캐릭터의 현재 좌표 변수 저장
@@ -108,6 +156,10 @@ while running:
   
   # 똥이 내려오는 속도
   ddong_y_pos += ddong_speed
+  ddong1_y_pos += ddong1_speed
+  ddong2_y_pos += ddong2_speed
+  
+  # 코인 내려오는 속도
   coin_y_pos += 5
   
   # 캐릭터가 배경화면 밖으로 나가지 않게 설정
@@ -125,6 +177,24 @@ while running:
     ddong_x_pos = randint(0, (screen_width - ddong_width))
     ddong_y_pos = 0
   
+  # 똥의 x 좌표가 배경화면 밖으로 나갈 경우 랜덤값으로 x좌표 재 설정 
+  if ddong1_x_pos < 0 or ddong1_x_pos > screen_width - ddong1_width:
+    ddong1_x_pos = randint(0, (screen_width - ddong1_width))
+  
+  # 똥의 y 좌표가 배경화면 밖으로 나갈 경우 랜덤값으로 x좌표 재설정 하고 y좌표는 0으로 재설정
+  if ddong1_y_pos > screen_height:
+    ddong1_x_pos = randint(0, (screen_width - ddong1_width))
+    ddong1_y_pos = 0
+    
+  # 똥의 x 좌표가 배경화면 밖으로 나갈 경우 랜덤값으로 x좌표 재 설정 
+  if ddong2_x_pos < 0 or ddong2_x_pos > screen_width - ddong2_width:
+    ddong2_x_pos = randint(0, (screen_width - ddong2_width))
+  
+  # 똥의 y 좌표가 배경화면 밖으로 나갈 경우 랜덤값으로 x좌표 재설정 하고 y좌표는 0으로 재설정
+  if ddong2_y_pos > screen_height:
+    ddong2_x_pos = randint(0, (screen_width - ddong2_width))
+    ddong2_y_pos = 0
+  
   # 코인의 y 좌표가 배경화면 밖으로 나갈 경우 랜덤값으로 x좌표 재설정 하고 y좌표는 0으로 재설정
   if coin_y_pos > screen_height:
     coin_x_pos = randint(0, (screen_width - ddong_width))
@@ -141,6 +211,16 @@ while running:
   ddong_rect.left = ddong_x_pos
   ddong_rect.top  = ddong_y_pos
   
+  # 똥의 현재 좌표
+  ddong1_rect = ddong1.get_rect()
+  ddong1_rect.left = ddong2_x_pos
+  ddong1_rect.top  = ddong2_y_pos
+  
+  # 똥의 현재 좌표
+  ddong2_rect = ddong2.get_rect()
+  ddong2_rect.left = ddong2_x_pos
+  ddong2_rect.top  = ddong2_y_pos
+  
   # 코인과 충돌하면 코인 1씩 증가
   coin_rect = coin.get_rect()
   coin_rect.left = coin_x_pos
@@ -149,10 +229,17 @@ while running:
   if character_rect.colliderect(ddong_rect):
     print("게임종료")
     running = False
-  elif character_rect.colliderect(coin_rect):
+  elif character_rect.colliderect(ddong1_rect):
+    print("게임종료")
+    running = False
+  elif character_rect.colliderect(ddong2_rect):
+    print("게임종료")
+    running = False
+  
+  if character_rect.colliderect(coin_rect):
     # 충돌하면 점수는 1씩 증가 시키고 새로 그려야함
     score += 1
-    coin_x_pos = randint(0, (screen_width - ddong_width))
+    coin_x_pos = randint(0, (screen_width - coin_width))
     coin_y_pos = 0
     
     
@@ -168,6 +255,9 @@ while running:
   
   # 똥 떨어지는 거 그리기
   screen.blit(ddong, (int(ddong_x_pos), int(ddong_y_pos)))
+  screen.blit(ddong1, (int(ddong1_x_pos), int(ddong1_y_pos)))
+  screen.blit(ddong2, (int(ddong2_x_pos), int(ddong2_y_pos)))
+  
   
   # 코인 그리기
   screen.blit(coin, (coin_x_pos, coin_y_pos))
